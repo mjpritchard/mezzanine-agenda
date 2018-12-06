50ed25300fd377626346c7aec1284f8d49949c78
mjpritchard <matt.pritchard@stfc.ac.uk>
2018-12-06 09:13:24 -0500

    Made urls.py consistent with blogs/urls.py

--- mezzanine_agenda/urls.py
+++ mezzanine_agenda/urls.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
+from . import views
 from mezzanine.conf import settings
 
 
@@ -9,51 +10,55 @@ from mezzanine.conf import settings
 _slash = "/" if settings.APPEND_SLASH else ""
 
 # Agenda patterns.
-urlpatterns = patterns("mezzanine_agenda.views",
+urlpatterns = [
     url("^feeds/(?P<format>.*)%s$" % _slash,
-        "event_feed", name="event_feed"),
+        views.event_feed, name="event_feed"),
     url("^tag/(?P<tag>.*)/feeds/(?P<format>.*)%s$" % _slash,
-        "event_feed", name="event_feed_tag"),
-    url("^tag/(?P<tag>.*)%s$" % _slash, "event_list",
-        name="event_list_tag"),
-    url("^tag/(?P<tag>.*)/calendar.ics$", "icalendar",
-        name="icalendar_tag"),
+        views.event_feed, name="event_feed_tag"),
+    url("^tag/(?P<tag>.*)%s$" % _slash,
+        views.event_list, name="event_list_tag"),
+    url("^tag/(?P<tag>.*)/calendar.ics$",
+        views.icalendar, name="icalendar_tag"),
     url("^location/(?P<location>.*)/feeds/(?P<format>.*)%s$" % _slash,
-        "event_feed", name="event_feed_location"),
+        views.event_feed, name="event_feed_location"),
     url("^location/(?P<location>.*)%s$" % _slash,
-        "event_list", name="event_list_location"),
+        views.event_list, name="event_list_location"),
     url("^location/(?P<location>.*)/calendar.ics$",
-        "icalendar", name="icalendar_location"),
+        views.icalendar, name="icalendar_location"),
     url("^author/(?P<username>.*)/feeds/(?P<format>.*)%s$" % _slash,
-        "event_feed", name="event_feed_author"),
+        views.event_feed, name="event_feed_author"),
     url("^author/(?P<username>.*)%s$" % _slash,
-        "event_list", name="event_list_author"),
+        views.event_list, name="event_list_author"),
     url("^author/(?P<username>.*)/calendar.ics$",
-        "icalendar", name="icalendar_author"),
+        views.icalendar, name="icalendar_author"),
     url("^archive/(?P<year>\d{4})/(?P<month>\d{1,2})%s$" % _slash,
-        "event_list", name="event_list_month"),
+        views.event_list, name="event_list_month"),
     url("^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/calendar.ics$",
-        "icalendar", name="icalendar_month"),
+        views.icalendar, name="icalendar_month"),
     url("^archive/(?P<year>\d{4})%s$" % _slash,
-        "event_list", name="event_list_year"),
+        views.event_list, name="event_list_year"),
     url("^archive/(?P<year>\d{4})/calendar.ics$",
-        "icalendar", name="icalendar_year"),
+        views.icalendar, name="icalendar_year"),
     url("^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/"
         "(?P<slug>.*)%s$" % _slash,
-        "event_detail", name="event_detail_day"),
+        views.event_detail, name="event_detail_day"),
     url("^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>.*)%s$" % _slash,
-        "event_detail", name="event_detail_month"),
+        views.event_detail, name="event_detail_month"),
     url("^(?P<year>\d{4})/(?P<slug>.*)%s$" % _slash,
-        "event_detail", name="event_detail_year"),
+        views.event_detail, name="event_detail_year"),
     url("^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/"
-        "(?P<slug>.*)/event.ics$", "icalendar_event", name="icalendar_event_day"),
+        "(?P<slug>.*)/event.ics$",
+        views.icalendar_event, name="icalendar_event_day"),
     url("^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<slug>.*)/event.ics$",
-        "icalendar_event", name="icalendar_event_month"),
+        views.icalendar_event, name="icalendar_event_month"),
     url("^(?P<year>\d{4})/(?P<slug>.*)/event.ics$",
-        "icalendar_event", name="icalendar_event_year"),
-    url("^(?P<slug>.*)/event.ics$", "icalendar_event", name="icalendar_event"),
-    url("^calendar.ics$", "icalendar", name="icalendar"),
-    url("^(?P<slug>.*)%s$" % _slash, "event_detail",
-        name="event_detail"),
-    url("^$", "event_list", name="event_list"),
-)
+        views.icalendar_event, name="icalendar_event_year"),
+    url("^(?P<slug>.*)/event.ics$",
+        views.icalendar_event, name="icalendar_event"),
+    url("^calendar.ics$",
+        views.icalendar, name="icalendar"),
+    url("^(?P<slug>.*)%s$" % _slash,
+        views.event_detail, name="event_detail"),
+    url("^$", 
+        views.event_list, name="event_list"),
+]
